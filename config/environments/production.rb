7c537917e2e2dd09bfdcfbd710fb6edd59e3d3ce
Bill Ramsey <bill@vybly.com>
2012-02-15 18:31:30 -0500

    made js files precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,6 +46,7 @@ CarrierwaveJqueryFileUpload::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile << '*.js'
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
