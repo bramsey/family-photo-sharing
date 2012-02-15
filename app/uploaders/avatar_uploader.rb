3249973f2888330e03bb94b3c6e45ae04c02dbdd
Bill Ramsey <bill@vybly.com>
2012-02-15 18:45:06 -0500

    added back some s3 paths

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -14,8 +14,8 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    #"uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
-    nil
+    "#{model.class.to_s.underscore}/#{model.id}"
+    #nil
   end
   
   process :resize_to_fill => [190, 190]
